de92470904b5a41a7c1291f21086407ff370263b
Jose Rosales <chemajose98@hotmail.com>
2026-06-04 22:11:19 -0600

    #CHANGE - Added momentary swagger

--- src/AquariumManager.Api/Program.cs
+++ src/AquariumManager.Api/Program.cs
@@ -174,7 +174,8 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+} else {app.UseSwagger();
+    app.UseSwaggerUI();}
 
 // Global error handler
 app.UseMiddleware<GlobalExceptionHandler>();
